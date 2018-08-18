c290eeba242bce6e9a33c1ebbd08ce0790e81a41
okta pilopa <pilopaokta@gmail.com>
2018-08-18 17:19:40 +0700

    bootstrap4m

---
+++ application/views/template_bootstrap4m/footer.php
@@ -0,0 +1,14 @@
+        </div>
+    </div>
+    <!-- JavaScript files-->
+    <script src="<?=base_url()?>assets_bootstrap4m/vendor/jquery/jquery.min.js"></script>
+    <script src="<?=base_url()?>assets_bootstrap4m/vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="<?=base_url()?>assets_bootstrap4m/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?=base_url()?>assets_bootstrap4m/vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="<?=base_url()?>assets_bootstrap4m/vendor/chart.js/Chart.min.js"></script>
+    <script src="<?=base_url()?>assets_bootstrap4m/vendor/jquery-validation/jquery.validate.min.js"></script>
+    <script src="<?=base_url()?>assets_bootstrap4m/js/charts-home.js"></script>
+    <!-- Main File-->
+    <script src="<?=base_url()?>assets_bootstrap4m/js/front.js"></script>
+  </body>
+</html>
